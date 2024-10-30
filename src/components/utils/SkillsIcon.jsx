23d2ff488e729bb1b6f322c4e62a0a79180c3512
longlin10086 <gnulonglin@gmail.com>
2024-10-30 15:41:31 +0800

    feat: redesign the skills module

--- src/components/utils/SkillsIcon.jsx
+++ src/components/utils/SkillsIcon.jsx
@@ -1,8 +1,18 @@
 function SkillsIcon({ skills }) {
-  let skills_str = skills.join(",");
   return (
-    <div className="my-2">
-      <img src={`https://skillicons.dev/icons?i=${skills_str}`} />
+    <div className="my-2 flex items-center flex-wrap">
+      {skills.map((skill) => {
+        return (
+          <div className="group flex justify-center items-center border-2 rounded-xl mx-1 mb-1 hover:shadow-lg hover:ring-2 hover:ring-blue-500/50 ease-in-out duration-500">
+            <img
+              src={`https://skillicons.dev/icons?i=${skill}&theme=light`}
+              width="25"
+              className="mx-1 my-1"
+            />
+            <div className="text-lg mb-2 ml-1 mr-2">{skill}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
