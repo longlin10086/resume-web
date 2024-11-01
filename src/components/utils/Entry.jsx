62bd5bda7c410e24ae30fc627cf73f50d3429f8f
longlin10086 <gnulonglin@gmail.com>
2024-11-01 12:34:02 +0800

    feat: finish en responsible design

--- src/components/utils/Entry.jsx
+++ src/components/utils/Entry.jsx
@@ -1,5 +1,6 @@
 import GhLink from "./GhLink";
 import { RiExternalLinkLine } from "react-icons/ri";
+import { useSwitchLang } from "./SwitchLang";
 
 function Entry({
   tl = "",
@@ -18,19 +19,21 @@ function Entry({
     remain_path = tr.replace(/https:\/\/github\.com\//, "");
   }
 
+  const { config, language, setLanguage } = useSwitchLang();
+
   return (
     <div className="w-full mt-1 flex-row justify-start items-center mb-2">
       <div className="flex justify-between items-start">
-        <div className="flex justify-start items-center flex-wrap overflow-auto">
-          <h2 className="font-bold text-nowrap">{tl}</h2>
+        <div className="flex justify-start items-center flex-wrap">
+          <h2 className="text-lg font-bold text-nowrap">{tl}</h2>
           <h2
             className={
-              tl_comment
+              tl_comment && language !== "en"
                 ? "before:content-['·'] before:mx-1 text-nowrap hidden sm:block text-sm"
                 : ""
             }
           >
-            {tl_comment}
+            {language !== "en" ? tl_comment : ""}
           </h2>
           {is_git ? (
             <a href={tr}>
@@ -44,18 +47,25 @@ function Entry({
           {is_git ? (
             <GhLink base_link={remain_path} className="hidden sm:flex" />
           ) : (
-            <p className="text-nowrap">{tr}</p>
+            <p className={`text-nowrap hidden sm:block`}>{tr}</p>
           )}
         </div>
       </div>
 
       <div className="flex justify-between items-center">
-        <h2 className="font-bold text-nowrap overflow-auto">{bl}</h2>
+        <h2
+          before={tr}
+          className={`text-md font-bold text-pretty ${
+            is_git ? "" : `after:content-[attr(before)] after:ml-2`
+          } sm:after:content-none`}
+        >
+          {bl}
+        </h2>
         <p>{br}</p>
       </div>
 
       {contents.length > 0 && (
-        <ul className="list-disc ml-4 text-balance">
+        <ul className="list-disc ml-4 text-pretty">
           {contents.map((content, index) => (
             <li key={index}>{content}</li>
           ))}
